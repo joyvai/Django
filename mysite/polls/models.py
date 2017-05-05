26f3409b182a0f36f629faba311c9fba3dcb22a7
iftekharul islam <iftekharjoy1@gmail.com>
2017-05-05 16:53:26 +0600

    Application folder
    Polls is my main application. This is a question based application.

---
+++ mysite/polls/models.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+
+import datetime
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
+# Create your models here.
+class Question(models.Model):
+    # these two are column name in database
+    # in python code i use this as a value
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # each_choice is releted to single question
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+            return self.choice_text
